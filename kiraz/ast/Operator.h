a4bc9cb8710698d562d1226214c818bdd974bcae
PawnPenguin <berkayyesilkaya@gmail.com>
2025-10-30 20:44:34 +0300

    HW1: Implement lexer and parser with arithmetic & comparison operators
    - Complete lexical analyzer
    
    - Parser grammar
    
    - AST operator nodes
    
    - File parsing mode implementation

--- kiraz/ast/Operator.h
+++ kiraz/ast/Operator.h
@@ -6,7 +6,230 @@
 #include <kiraz/Node.h>
 
 namespace ast {
-// TODO
+
+class BinaryOp : public Node {
+public:
+    BinaryOp(Node::Ptr left, Node::Ptr right) 
+        : m_left(left), m_right(right) {}
+    
+    Node::Ptr get_left() const { return m_left; }
+    Node::Ptr get_right() const { return m_right; }
+    
+protected:
+    Node::Ptr m_left;
+    Node::Ptr m_right;
+};
+
+class Add : public BinaryOp {
+public:
+    Add(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("Add(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class Sub : public BinaryOp {
+public:
+    Sub(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("Sub(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class Mult : public BinaryOp {
+public:
+    Mult(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("Mult(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class Div : public BinaryOp {
+public:
+    Div(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("Divf(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class OpEq : public BinaryOp {
+public:
+    OpEq(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("OpEq(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class OpNe : public BinaryOp {
+public:
+    OpNe(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("OpNe(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class OpLt : public BinaryOp {
+public:
+    OpLt(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("OpLt(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class OpGt : public BinaryOp {
+public:
+    OpGt(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("OpGt(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class OpLe : public BinaryOp {
+public:
+    OpLe(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("OpLe(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class OpGe : public BinaryOp {
+public:
+    OpGe(Node::Ptr left, Node::Ptr right) : BinaryOp(left, right) {}
+    
+    std::string as_string() const override {
+        return fmt::format("OpGe(l={}, r={})", m_left->as_string(), m_right->as_string());
+    }
+};
+
+class Let : public Node {
+public:
+    Let(Node::Ptr name, Node::Ptr type, Node::Ptr init) 
+        : m_name(name), m_type(type), m_init(init) {}
+    
+    std::string as_string() const override {
+        std::string result = "Let(n=" + m_name->as_string();
+        if (m_type) {
+            result += ", t=" + m_type->as_string();
+        }
+        if (m_init) {
+            result += ", i=" + m_init->as_string();
+        }
+        result += ")";
+        return result;
+    }
+    
+private:
+    Node::Ptr m_name;
+    Node::Ptr m_type;
+    Node::Ptr m_init;
+};
+
+class FArg : public Node {
+public:
+    FArg(Node::Ptr name, Node::Ptr type) 
+        : m_name(name), m_type(type) {}
+    
+    std::string as_string() const override {
+        return fmt::format("FArg(n={}, t={})", m_name->as_string(), m_type->as_string());
+    }
+    
+private:
+    Node::Ptr m_name;
+    Node::Ptr m_type;
+};
+
+class FuncArgs : public Node {
+public:
+    FuncArgs(std::vector<Node::Ptr> args) : m_args(args) {}
+    
+    std::string as_string() const override {
+        std::string result = "[";
+        bool first = true;
+        for (const auto &arg : m_args) {
+            if (!first) result += ", ";
+            first = false;
+            result += arg->as_string();
+        }
+        result += "]";
+        return result;
+    }
+    
+    bool is_funcarg_list() const override { return true; }
+    
+private:
+    std::vector<Node::Ptr> m_args;
+};
+
+class StmtList : public Node {
+public:
+    StmtList(std::vector<Node::Ptr> stmts) : m_stmts(stmts) {}
+    
+    std::string as_string() const override {
+        std::string result = "[";
+        bool first = true;
+        for (const auto &stmt : m_stmts) {
+            if (!first) result += ", ";
+            first = false;
+            result += stmt->as_string();
+        }
+        result += "]";
+        return result;
+    }
+    
+    bool is_stmt_list() const override { return true; }
+    
+    void add(Node::Ptr stmt) { m_stmts.push_back(stmt); }
+    const std::vector<Node::Ptr>& get_stmts() const { return m_stmts; }
+    
+private:
+    std::vector<Node::Ptr> m_stmts;
+};
+
+class Func : public Node {
+public:
+    Func(Node::Ptr name, Node::Ptr args, Node::Ptr ret_type, Node::Ptr scope) 
+        : m_name(name), m_args(args), m_ret_type(ret_type), m_scope(scope) {}
+    
+    std::string as_string() const override {
+        return fmt::format("Func(n={}, a={}, r={}, s={})", 
+                          m_name->as_string(), 
+                          m_args->as_string(), 
+                          m_ret_type->as_string(),
+                          m_scope->as_string());
+    }
+    
+    bool is_func() const override { return true; }
+    
+private:
+    Node::Ptr m_name;
+    Node::Ptr m_args;
+    Node::Ptr m_ret_type;
+    Node::Ptr m_scope;
+};
+
+class Assignment : public Node {
+public:
+    Assignment(Node::Ptr name, Node::Ptr value) 
+        : m_name(name), m_value(value) {}
+    
+    std::string as_string() const override {
+        return fmt::format("Assign(n={}, v={})", m_name->as_string(), m_value->as_string());
+    }
+    
+private:
+    Node::Ptr m_name;
+    Node::Ptr m_value;
+};
+
 }
 
 #endif // KIRAZ_AST_OPERATOR_H
--- parser.yy
+++ parser.yy
@@ -13,13 +13,152 @@ extern int yylineno;
 
 %token    REJECTED
 
-/* TODO */
+/* Keywords */
+%token    KW_IMPORT
+%token    KW_FUNC
+%token    KW_IF
+%token    KW_ELSE
+%token    KW_WHILE
+%token    KW_CLASS
+%token    KW_LET
+%token    KW_RETURN
+
+/* Literals and Identifiers */
+%token    L_INTEGER
+%token    L_STRING
+%token    IDENTIFIER
+
+/* Operators and Punctuation */
+%token    OP_LBRACE      /* { */
+%token    OP_RBRACE      /* } */
+%token    OP_LPAREN      /* ( */
+%token    OP_RPAREN      /* ) */
+%token    OP_PLUS        /* + */
+%token    OP_MINUS       /* - */
+%token    OP_MULT        /* * */
+%token    OP_DIV         /* / */
+%token    OP_LT          /* < */
+%token    OP_GT          /* > */
+%token    OP_EQ          /* == */
+%token    OP_NE          /* != */
+%token    OP_LE          /* <= */
+%token    OP_GE          /* >= */
+%token    OP_NOT         /* ! */
+%token    OP_ASSIGN      /* = */
+%token    OP_COLON       /* : */
+%token    OP_SCOLON      /* ; */
+%token    OP_COMMA       /* , */
+
+/* Operator precedence (lowest to highest) */
+%left OP_EQ OP_NE OP_LT OP_GT OP_LE OP_GE  /* Comparison operators */
+%left OP_PLUS OP_MINUS
+%left OP_MULT OP_DIV
+%nonassoc UMINUS  /* Unary minus - higher precedence */
 
 %%
 
-/* TODO */
+/* Program entry point - a single statement */
+stmt: func_stmt                 { Node::add<ast::StmtList>(std::vector{$1}); }
+    | func_stmt OP_SCOLON       { Node::add<ast::StmtList>(std::vector{$1}); }
+    | let_stmt OP_SCOLON        { Node::add<ast::StmtList>(std::vector{$1}); }
+    | assignment_stmt OP_SCOLON { Node::add<ast::StmtList>(std::vector{$1}); }
+    | expr OP_SCOLON            { Node::add<ast::StmtList>(std::vector{$1}); }
+    | REJECTED                  { yyerror("Rejected token"); }
+    ;
 
-stmt: %empty
+/* Function definition */
+func_stmt: KW_FUNC IDENTIFIER OP_LPAREN func_args OP_RPAREN OP_COLON type_annotation OP_LBRACE stmt_list OP_RBRACE {
+        $$ = Node::add<ast::Func>($2, $4, $7, $9);
+      }
+    ;
+
+/* Function arguments */
+func_args: /* empty */          { $$ = std::make_shared<ast::FuncArgs>(std::vector<Node::Ptr>{}); }
+    | func_arg_list             { $$ = std::make_shared<ast::FuncArgs>(std::vector{$1}); }
+    ;
+
+func_arg_list: func_arg                         { $$ = $1; }
+    | func_arg_list OP_COMMA func_arg           { $$ = $3; }
+    ;
+
+func_arg: IDENTIFIER OP_COLON type_annotation   {
+        $$ = std::make_shared<ast::FArg>($1, $3);
+      }
+    ;
+
+/* Type annotation */
+type_annotation: IDENTIFIER     {
+        $$ = $1;
+      }
+    ;
+
+/* Let statement */
+let_stmt: KW_LET IDENTIFIER {
+        $$ = std::make_shared<ast::Let>($2, nullptr, nullptr);
+      }
+    | KW_LET IDENTIFIER OP_ASSIGN expr {
+        $$ = std::make_shared<ast::Let>($2, nullptr, $4);
+      }
+    | KW_LET IDENTIFIER OP_COLON type_annotation {
+        $$ = std::make_shared<ast::Let>($2, $4, nullptr);
+      }
+    | KW_LET IDENTIFIER OP_COLON type_annotation OP_ASSIGN expr {
+        $$ = std::make_shared<ast::Let>($2, $4, $6);
+      }
+    ;
+
+/* Assignment statement */
+assignment_stmt: IDENTIFIER OP_ASSIGN expr {
+        $$ = std::make_shared<ast::Assignment>($1, $3);
+      }
+    ;
+
+/* Statement list (for function body) */
+stmt_list: /* empty */                    { $$ = std::make_shared<ast::StmtList>(std::vector<Node::Ptr>{}); }
+    | stmt_list let_stmt OP_SCOLON        { 
+        auto list = std::dynamic_pointer_cast<ast::StmtList>($1);
+        list->add($2);
+        $$ = $1;
+      }
+    | stmt_list assignment_stmt OP_SCOLON { 
+        auto list = std::dynamic_pointer_cast<ast::StmtList>($1);
+        list->add($2);
+        $$ = $1;
+      }
+    | stmt_list expr OP_SCOLON            { 
+        auto list = std::dynamic_pointer_cast<ast::StmtList>($1);
+        list->add($2);
+        $$ = $1;
+      }
+    | stmt_list func_stmt                 { 
+        auto list = std::dynamic_pointer_cast<ast::StmtList>($1);
+        list->add($2);
+        $$ = $1;
+      }
+    | stmt_list func_stmt OP_SCOLON      { 
+        auto list = std::dynamic_pointer_cast<ast::StmtList>($1);
+        list->add($2);
+        $$ = $1;
+      }
+    ;
+
+/* Expressions */
+expr: L_INTEGER                  { $$ = $1; }
+    | L_STRING                   { $$ = $1; }
+    | IDENTIFIER                 { $$ = $1; }
+    | OP_LPAREN expr OP_RPAREN   { $$ = $2; }
+    | OP_MINUS expr %prec UMINUS { $$ = std::make_shared<ast::Signed>("OP_MINUS", $2); }
+    | expr OP_PLUS expr          { $$ = std::make_shared<ast::Add>($1, $3); }
+    | expr OP_MINUS expr         { $$ = std::make_shared<ast::Sub>($1, $3); }
+    | expr OP_MULT expr          { $$ = std::make_shared<ast::Mult>($1, $3); }
+    | expr OP_DIV expr           { $$ = std::make_shared<ast::Div>($1, $3); }
+    | expr OP_EQ expr            { $$ = std::make_shared<ast::OpEq>($1, $3); }
+    | expr OP_NE expr            { $$ = std::make_shared<ast::OpNe>($1, $3); }
+    | expr OP_LT expr            { $$ = std::make_shared<ast::OpLt>($1, $3); }
+    | expr OP_GT expr            { $$ = std::make_shared<ast::OpGt>($1, $3); }
+    | expr OP_LE expr            { $$ = std::make_shared<ast::OpLe>($1, $3); }
+    | expr OP_GE expr            { $$ = std::make_shared<ast::OpGe>($1, $3); }
+    ;
 
 %%
 
